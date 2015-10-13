bac87ef195961bd33b798fad1613699cc64da289
Xett575 <Schroderthefirst@gmail.com>
2015-10-13 16:02:59 -0400

    Simple Visual Basic Journal Program

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles Button1.Click
+        SaveFileDialog1.Filter = "All Files|*.*"
+        SaveFileDialog1.ShowDialog()
+        RichTextBox1.SaveFile(SaveFileDialog1.FileName, RichTextBoxStreamType.PlainText)
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As System.Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+
+    End Sub
+End Class
